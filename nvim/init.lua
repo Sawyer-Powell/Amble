6a9d405a556f475b5cda161f34d3ce840d845793
Sawyer Powell <sawyerhpowell@gmail.com>
2024-09-24 18:13:48 -0700

    Added nvim extension submodules

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,5 +1,7 @@
-local amble = require('amble')
+vim.o.runtimepath = vim.o.runtimepath .. ",./nvim/third-party/telescope.nvim"
+vim.o.runtimepath = vim.o.runtimepath .. ",./nvim/third-party/plenary.nvim"
 
+local amble = require('amble')
 amble.init()
 
 vim.g.mapleader = " "
@@ -7,3 +9,10 @@ vim.g.mapleader = " "
 vim.api.nvim_set_keymap('n', '<leader>q', ':q!<CR>', {})
 vim.api.nvim_set_keymap('n', '<leader>al', ':AmbleList<CR>', {})
 vim.api.nvim_set_keymap('n', '<leader>an', ':AmbleNew<CR>', {})
+
+local builtin = require('telescope.builtin')
+
+vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
